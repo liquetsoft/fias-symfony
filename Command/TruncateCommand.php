db8787fcfc919ed456f9063b8fadc280e8775773
liquetsoft <liquetsoft@protonmail.com>
2019-05-13 18:29:33 +0500

    add TruncateCommand

---
+++ Command/TruncateCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Command;
+
+use Liquetsoft\Fias\Component\Pipeline\Task\Task;
+use Liquetsoft\Fias\Component\Pipeline\State\ArrayState;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Команда, которая очищает хранилища для всех сущностей проекта, привязанных
+ * к сущностям ФИАС.
+ */
+class TruncateCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'liquetsoft:fias:truncate';
+
+    /**
+     * @var Task
+     */
+    protected $truncateTask;
+
+    /**
+     * @param Task $truncateTask
+     */
+    public function __construct(Task $truncateTask)
+    {
+        $this->truncateTask = $truncateTask;
+
+        parent::__construct();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure(): void
+    {
+        $this->setDescription('Truncates storages for binded entities.');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->note('Truncating storage for binded entities.');
+
+        $state = new ArrayState;
+        $this->truncateTask->run($state);
+
+        $io->success('Storages truncated.');
+    }
+}
