e5596a2d569b7420f15f88454b7567d0529ee679
liquetsoft <liquetsoft@protonmail.com>
2019-05-14 14:50:00 +0500

    add llop to update command

--- Command/UpdateCommand.php
+++ Command/UpdateCommand.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Command;
 
 use Liquetsoft\Fias\Component\Pipeline\Pipe\Pipe;
+use Liquetsoft\Fias\Component\Pipeline\Task\Task;
 use Liquetsoft\Fias\Component\Pipeline\State\ArrayState;
+use Liquetsoft\Fias\Component\FiasInformer\InformerResponse;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use RuntimeException;
 
 /**
  * Команда, которая обновляет ФИАС с текущей версии до самой свежей.
@@ -54,8 +57,21 @@ class UpdateCommand extends Command
         $io->note('Updating FIAS.');
         $start = microtime(true);
 
-        $state = new ArrayState;
-        $this->pipeline->run($state);
+        do {
+            $state = new ArrayState;
+            $this->pipeline->run($state);
+
+            $info = $state->getParameter(Task::FIAS_INFO_PARAM);
+            if (!($info instanceof InformerResponse)) {
+                throw new RuntimeException(
+                    "There is no '" . Task::FIAS_INFO_PARAM . "' parameter in state."
+                );
+            }
+
+            if ($info->hasResult()) {
+                $io->note("Updated to version '{$info->getVersion()}'.");
+            }
+        } while ($info->hasResult());
 
         $total = round(microtime(true) - $start, 4);
         $io->success("FIAS updated after {$total} s.");
