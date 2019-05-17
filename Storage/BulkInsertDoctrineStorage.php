0be1cb5d373a9751e90a0ab387938a79e0540b66
liquetsoft <liquetsoft@protonmail.com>
2019-05-17 12:47:31 +0500

    add BulkInsertDoctrineStorage

---
+++ Storage/BulkInsertDoctrineStorage.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Storage;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
+use Ramsey\Uuid\UuidInterface;
+use DateTimeInterface;
+use RuntimeException;
+
+/**
+ * Объект, который сохраняет данные ФИАС с помощью Doctrine.
+ *
+ * В отличие от базового объекта использует для вставки bulk (множественный) insert.
+ * Следует понимать, что события и другие фичи Doctrine, связанные с сущностями
+ * при такой вставке не работают. Кроме того, данная реализация подходит не для всех СУБД.
+ */
+class BulkInsertDoctrineStorage extends DoctrineStorage
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * Сохраненные в памяти данные для мнодественной вставки.
+     *
+     * Массив вида 'имя таблицы' => 'массив массивов данных для вставки'.
+     *
+     * @var mixed[]
+     */
+    protected $insertData = [];
+
+    /**
+     * @param ManagerRegistry $doctrine
+     * @param int             $insertBatch
+     */
+    public function __construct(ManagerRegistry $doctrine, int $insertBatch = 800)
+    {
+        $em = $doctrine->getManager();
+        if (!($em instanceof EntityManager)) {
+            throw new RuntimeException(
+                "Bulk insert can only be used with '" . EntityManager::class . "'"
+            );
+        }
+        $this->em = $em;
+
+        $this->insertBatch = $insertBatch;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function stop(): void
+    {
+        parent::stop();
+        $this->checkAndFlushInsert(true);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function insert(object $entity): void
+    {
+        $meta = $this->em->getClassMetadata(get_class($entity));
+
+        $table = $meta->getTableName();
+        $fileds = $meta->getFieldNames();
+
+        $insertArray = [];
+        foreach ($fileds as $field) {
+            $value = $meta->getFieldValue($entity, $field);
+            if ($value instanceof DateTimeInterface) {
+                $value = $value->format('Y-m-d H:i:s');
+            } elseif ($value instanceof UuidInterface) {
+                $value = $value->toString();
+            }
+            $column = $meta->getColumnName($field);
+            $insertArray[$column] = $value;
+        }
+
+        $this->insertData[$table][] = $insertArray;
+        $this->checkAndFlushInsert(false);
+    }
+
+    /**
+     * Проверяет нужно ли отправлять запросы на множественные вставки элементов,
+     * сохраненых в памяти.
+     *
+     * @param bool $forceInsert
+     */
+    protected function checkAndFlushInsert(bool $forceInsert = false): void
+    {
+        foreach ($this->insertData as $tableName => $insertData) {
+            if ($forceInsert || count($insertData) >= $this->insertBatch) {
+                $this->bulkInsert($tableName, $insertData);
+                unset($this->insertData[$tableName]);
+            }
+        }
+    }
+
+    /**
+     * Отправляет запрос на массовую вставку данных в таблицу.
+     *
+     * @param string  $tableName
+     * @param mixed[] $data
+     *
+     * @throws RuntimeException
+     */
+    protected function bulkInsert(string $tableName, array $data): void
+    {
+        $dataSample = reset($data);
+
+        $paramNames = implode(', ', array_keys($dataSample));
+        $paramValues = implode(', ', array_fill(0, count($dataSample), '?'));
+        $dataValues = '(' . implode('), (', array_fill(0, count($data), $paramValues)) . ')';
+        $sql = "INSERT INTO {$tableName} ({$paramNames}) VALUES {$dataValues}";
+
+        $stmt = $this->em->getConnection()->prepare($sql);
+        $count = 0;
+        foreach ($data as $item) {
+            foreach ($item as $value) {
+                $stmt->bindValue(++$count, $value);
+            }
+        }
+
+        $stmt->execute();
+    }
+}
