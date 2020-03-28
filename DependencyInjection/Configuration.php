db359b43e4bf824ae0ba578026cbfe84ee295247
marvin255 <postbox433@gmail.com>
2020-03-28 10:57:10 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -6,6 +6,7 @@ namespace Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Класс с описанием настроек бандла.
@@ -21,16 +22,17 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder(self::CONFIG_NAME);
+        $rootDir = $this->getPathToRootDir();
 
+        $treeBuilder = new TreeBuilder(self::CONFIG_NAME);
         $treeBuilder->getRootNode()
             ->children()
                 ->scalarNode('informer_wsdl')->defaultValue('http://fias.nalog.ru/WebServices/Public/DownloadService.asmx?WSDL')->end()
-                ->scalarNode('registry_yaml')->defaultValue('%kernel.root_dir%/../vendor/liquetsoft/fias-component/resources/fias_entities.yaml')->end()
+                ->scalarNode('registry_yaml')->defaultValue("{$rootDir}/vendor/liquetsoft/fias-component/resources/fias_entities.yaml")->end()
                 ->scalarNode('temp_dir')->defaultValue('%kernel.cache_dir%/fias')->end()
                 ->scalarNode('version_manager_entity')->defaultValue('')->end()
                 ->scalarNode('insert_batch_count')->defaultValue(800)->end()
-                ->scalarNode('path_to_console_bin')->defaultValue('%kernel.root_dir%/../bin/console')->end()
+                ->scalarNode('path_to_console_bin')->defaultValue("{$rootDir}/bin/console")->end()
                 ->scalarNode('number_of_parallel')->defaultValue(5)->end()
                 ->scalarNode('paralleling_running_command')->defaultValue('liquetsoft:fias:install_parallel_running')->end()
                 ->arrayNode('entity_bindings')
@@ -42,5 +44,17 @@ class Configuration implements ConfigurationInterface
         ;
 
         return $treeBuilder;
+    }
+
+    /**
+     * Returns path to project root dir.
+     *
+     * @return string
+     */
+    private function getPathToRootDir()
+    {
+        return version_compare(Kernel::VERSION, '5.0.0', '<')
+            ? '%kernel.root_dir%/..'
+            : '%kernel.project_dir%';
     }
 }
