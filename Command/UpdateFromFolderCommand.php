46bac2770742eb404c12867377eb736168b8836d
marvin255 <postbox433@gmail.com>
2020-09-17 22:22:19 +0200

    add UpdateFromFolderCommand (#18)

---
+++ Command/UpdateFromFolderCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Command;
+
+use Liquetsoft\Fias\Component\Pipeline\Pipe\Pipe;
+use Liquetsoft\Fias\Component\Pipeline\State\ArrayState;
+use Liquetsoft\Fias\Component\Pipeline\Task\Task;
+use SplFileInfo;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Команда, которая запускает обновление ФИАС из xml файлов,
+ * сохраненных на локальном диске.
+ */
+class UpdateFromFolderCommand extends Command
+{
+    protected static $defaultName = 'liquetsoft:fias:update_from_folder';
+
+    protected Pipe $pipeline;
+
+    public function __construct(Pipe $pipeline)
+    {
+        $this->pipeline = $pipeline;
+
+        parent::__construct();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Updates version of FIAS from set folder.')
+            ->addArgument('folder', InputArgument::REQUIRED, 'Path to folder on local system with FIAS xmls.')
+        ;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $folder = $input->getArgument('folder');
+        if (is_array($folder)) {
+            $folder = reset($folder);
+        }
+        $folder = (string) $folder;
+
+        $io->note("Updating version of FIAS from '{$folder}' folder.");
+
+        $state = new ArrayState();
+        $state->setAndLockParameter(Task::EXTRACT_TO_FOLDER_PARAM, new SplFileInfo($folder));
+        $this->pipeline->run($state);
+
+        $io->success("FIAS updated from '{$folder}' folder.");
+
+        return 0;
+    }
+}
