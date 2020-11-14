b7d640ddd6fbd8ad90f77e6be6be84e6ace5fdbc
marvin255 <postbox433@gmail.com>
2020-11-14 12:08:07 +0100

    code style fixes

--- Command/StatusCheckCommand.php
+++ Command/StatusCheckCommand.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Command;
 
-use Liquetsoft\Fias\Component\FiasInformer\FiasInformer;
-use Liquetsoft\Fias\Component\FiasInformer\InformerResponse;
-use Liquetsoft\Fias\Component\VersionManager\VersionManager;
+use Liquetsoft\Fias\Component\FiasStatusChecker\FiasStatusChecker;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Liquetsoft\Fias\Component\FiasStatusChecker\FiasStatusChecker;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
@@ -47,9 +44,9 @@ class StatusCheckCommand extends Command
 
         $status = $this->checker->check();
         if ($status->getResultStatus() === FiasStatusChecker::STATUS_AVAILABLE) {
-            $io->success("FIAS is OK and available.");
+            $io->success('FIAS is OK and available.');
         } else {
-            $io->error("FIAS is not available.");
+            $io->error('FIAS is not available.');
             $table = new Table($output);
             $table->setColumnWidths([15, 15, 60]);
             $table->setHeaders(['Service', 'Status', 'Reason'])->setRows($status->getPerServiceStatuses());
