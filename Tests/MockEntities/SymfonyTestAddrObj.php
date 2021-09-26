a6af31b759a5a13c28c9b9e1bacc598fe3bd5488
marvin255 <postbox433@gmail.com>
2021-09-26 14:48:34 +0200

    Symfony tests (#39)

---
+++ Tests/MockEntities/SymfonyTestAddrObj.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Entity\AddrObj as LiquetsoftEntity;
+
+/**
+ * @ORM\Entity()
+ */
+class AddrObj extends LiquetsoftEntity
+{
+}
