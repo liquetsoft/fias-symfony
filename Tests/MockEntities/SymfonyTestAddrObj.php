c710da2b8e50edc093090594dfc8f79d8642624b
marvin255 <postbox433@gmail.com>
2021-09-26 13:35:55 +0200

    add new symfony workflow and test files for it

---
+++ Tests/MockEntities/SymfonyTestAddrObj.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Entity\AddrObj as LiquetsoftEntity;
+
+/**
+ * @ORM\Entity()
+ */
+class AddrObj extends LiquetsoftEntity
+{
+}
---
+++ Tests/MockEntities/SymfonyTestFiasVersion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Liquetsoft\Fias\Symfony\LiquetsoftFiasBundle\Entity\FiasVersion as LiquetsoftEntity;
+
+/**
+ * @ORM\Entity()
+ */
+class FiasVersion extends LiquetsoftEntity
+{
+}
